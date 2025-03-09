134f86e46358d3855f3b5e9b17eeb7172218bfa1
iceywth <131197703+acezqqq@users.noreply.github.com>
2025-03-09 18:14:16 +0100

    Update PremLoader.lua

--- PremLoader.lua
+++ PremLoader.lua
@@ -32,6 +32,8 @@ local UI = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/rele
 local API = loadstring(game:HttpGet("https://sdkAPI-public.luarmor.net/library.lua"))()
 if game.placeId == 18668065416 then
     API.script_id = "14d860e5b3f0a71c45ac877e07177807"
+elseif game.placeId == 8075399143 then
+	API.script_id = "b225e49d25df5af086c6c6a530ee9d75"
 else
     game.Players.LocalPlayer:Kick("Scylla doesn't support this game | Join our discord for more information")
 end
