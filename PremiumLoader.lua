1d70789fecf2fd49f86bfd3251e755dbf642dbe3
scylla <131197703+acezqqq@users.noreply.github.com>
2025-08-21 22:51:10 +0200

    Update PremiumLoader.lua

--- PremiumLoader.lua
+++ PremiumLoader.lua
@@ -20,11 +20,11 @@ local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/acezqqq/mo
 local API = loadstring(game:HttpGet("https://sdkAPI-public.luarmor.net/library.lua"))()
 
 local games = {
-	[TypeSoul] = "4871329703",
-	[DIG] = "7218065222",
-	[BLR] = "6325068386",
-	[GhoulRe] = "6490954291",
-	[GAG] = "7436755782"
+	[TypeSoul] = 4871329703,
+	[DIG] = 7218065222,
+	[BLR] = 6325068386,
+	[GhoulRe] = 6490954291,
+	[GAG] = 7436755782
 }
 
 local GAME_SCRIPTS = {
